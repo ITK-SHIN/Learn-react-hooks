b3f672a01a8f0f62eba98081e2ce710752114ff0
신상우 <bano112@naver.com>
2023-07-13 02:48:45 +0900

    'useState연습'

---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,36 @@
+import { Fragment, useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onChangeName = (e) => {
+    console.log(e);
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+      </div>
+      <div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Info;
